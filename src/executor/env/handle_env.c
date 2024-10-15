118696ee001c2d191ea0155e8d256c83a977bdd7
Beatriz <beatrizoffline@gmail.com>
2024-10-15 15:50:39 +0100

    not print env vazias

--- src/executor/env/handle_env.c
+++ src/executor/env/handle_env.c
@@ -1,5 +1,19 @@
 #include "minishell.h"
 
+static int has_value(char *env)
+{
+	int i;
+
+	i = 0;
+	while (env[i])
+	{
+		if (env[i] == '=')
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
 static int	check_env(t_tokens *tokens, t_shell *shell)
 {
 	if (ft_strncmp(tokens->token, "env", 4) != 0)
@@ -24,7 +38,8 @@ int	ft_env(t_shell *shell, t_tokens *tokens)
 	envp = shell->envp;
 	while (*envp)
 	{
-		printf("%s\n", *envp);
+		if (has_value(*envp))
+			printf("%s\n", *envp);
 		envp++;
 	}
 	return (1);
